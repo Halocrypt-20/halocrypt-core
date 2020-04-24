2790bd1c66231b5a97330520148d43251d6cdd7f
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2020-04-24 22:19:54 +0530

    Fixed Mail Helper

--- api_handlers/email_manager.py
+++ api_handlers/email_manager.py
@@ -1,13 +1,14 @@
-from smtplib import SMTP
-from os import environ
+import smtplib
+import os
 
-def send_email_to_user(init, email, subject, content):
-    with SMTP('smtp.gmail.com', 587) as smtp:
+def send_email_to_user(email: str, subject: str, body: str):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.ehlo()
         smtp.starttls()
         smtp.ehlo()
 
-        smtp.login(environ.get("MAIL_USERNAME"), environ.get("MAIL_PASSWORD"))
-        message = f'Subject: {subject}\n\n{content}'
-        smtp.sendmail(environ.get("MAIL_USERNAME"), email, message)
-        return str("Message sent!")
+        smtp.login(os.environ("SMTP_USER"), os.environ("SMTP_PASS"))
+
+        msgw = f'Subject: {subject}\n\n{body}'
+
+        smtp.sendmail(os.environ("SMTP_USER"), email, msgw)
