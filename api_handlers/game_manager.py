492a77a190749c7ca59189bc5eb26074734ab790
me <bhavesh.pareek36@gmail.com>
2020-05-03 20:10:31 +0530

    emojis :/

--- api_handlers/game_manager.py
+++ api_handlers/game_manager.py
@@ -83,9 +83,10 @@ def answer_question(question_number: int, answer: str, user: UserTable) -> dict:
         return no_question(question_number)
     correct = replace("", question.answer)
     current = replace("", answer)
-    js = f"{user.user} tried to answer {user.current_level} with {current}  (correct answer - {correct})"
+    is_correct = correct == current 
+    js = f"{user.user} tried to answer {user.current_level} with {current}  ({"✅" if is_correct else "❌"})"
     post_level_up_webhook(js)
-    if correct == current:  # no
+    if is_correct:  # no
         user.current_level = user.current_level + 1  # +=1 yeah
         user.last_question_answered_at = js_time()
         save_to_db()
